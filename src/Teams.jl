847c85d50e29838cd9496be35cce3d44e64d56df
Conrad Wiebe <cwiebe@lutron.com>
2023-06-01 09:27:57 -0400

    initial commit

--- src/Teams.jl
+++ src/Teams.jl
@@ -1,5 +1,36 @@
 module Teams
 
-# Write your package code here.
+using HTTP, JSON
 
+export GetUserData, GetUsersTeams, GetTeamMembers
+
+function GetUserData(access_token::AbstractString)::Dict{String, Any}
+    resp = HTTP.get("https://graph.microsoft.com/v1.0/me", ["Authorization"=>"Bearer $(access_token)"])
+    body = JSON.parse(String(resp.body))
+    body
+end
+
+function GetUserData(access_token::AbstractString, user_id::AbstractString)::Dict{String, Any}
+    resp = HTTP.get("https://graph.microsoft.com/v1.0/$(user_id)", ["Authorization"=>"Bearer $(access_token)"])
+    body = JSON.parse(String(resp.body))
+    body
+end
+
+function GetUsersTeams(access_token::AbstractString)::Dict{String, Any}
+    resp = HTTP.get("https://graph.microsoft.com/v1.0/me/joinedTeams", ["Authorization"=>"Bearer $(access_token)"])
+    body = JSON.parse(String(resp.body))
+    body
+end
+
+function GetUsersTeams(access_token::AbstractString, user_id::AbstractString)::Dict{String, Any}
+    resp = HTTP.get("https://graph.microsoft.com/v1.0/$(user_id)/joinedTeams", ["Authorization"=>"Bearer $(access_token)"])
+    body = JSON.parse(String(resp.body))
+    body
+end
+
+function GetTeamMembers(access_token::AbstractString, team_id::AbstractString)::Dict{String, Any}
+    resp = HTTP.get("https://graph.microsoft.com/v1.0/groups/$(team_id)/members", ["Authorization"=>"Bearer $(access_token)"])
+    body = JSON.parse(String(resp.body))
+    body
+end
 end
