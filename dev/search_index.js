160dfd0e9af1f0456dd01683a1c5bf2b28d9d00b
Documenter.jl <documenter@juliadocs.github.io>
2023-06-01 13:36:19 +0000

    build based on 847c85d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Teams","category":"page"},{"location":"#Teams","page":"Home","title":"Teams","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Teams.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Teams]","category":"page"}]
+}
